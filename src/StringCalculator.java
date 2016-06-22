567cb14b4e9c875b538bb53361e28239b9ecceb3
HFCherish <hf_cherish@163.com>
2016-06-22 16:15:32 +0800

    supported sum more numbers, with specified delimiter, and detect negatives

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,10 +1,28 @@
 public class StringCalculator {
-    public static int calculate(String numbers) {
+    public static int calculate(String numbers) throws Exception {
         if (numbers == null || numbers.length() == 0) return 0;
-        String[] numbs = numbers.split(",");
+        String delimiter = "[,|\n]";
+        String[] delimiterNumbers = numbers.split("\n");
+        if (delimiterNumbers.length > 1) {
+            try {
+                Integer.valueOf(delimiterNumbers[0]);
+            } catch (NumberFormatException exception) {
+                delimiter = delimiterNumbers[0];
+                numbers = delimiterNumbers[1];
+            }
+        }
+        String[] numbs = numbers.split(delimiter);
         int sum = 0;
-        for( String numb: numbs ) {
+        StringBuilder negativeNumbers = new StringBuilder();
+        for (String numb : numbs) {
+            if (numb.charAt(0) == '-') {
+                negativeNumbers.append(numb + ", ");
+                continue;
+            }
             sum += Integer.valueOf(numb);
+        }
+        if (negativeNumbers.length() > 0) {
+            throw new Exception("negatives not allowed: " + negativeNumbers.substring(0,negativeNumbers.length()-2));
         }
         return sum;
     }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,28 +1,67 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class StringCalculatorTest {
 
     @Test
-    public void shouldReturn0GivenEmptyString() {
+    public void shouldReturn0GivenEmptyString() throws Exception {
         assertThat(StringCalculator.calculate(""), is(0));
         assertThat(StringCalculator.calculate(null), is(0));
     }
 
     @Test
-    public void shouldReturnNumberGivenOneNumberString() {
+    public void shouldReturnNumberGivenOneNumberString() throws Exception {
         assertThat(StringCalculator.calculate("1"), is(1));
-        assertThat(StringCalculator.calculate("-1"), is(-1));
         assertThat(StringCalculator.calculate("123"), is(123));
 
     }
 
     @Test
-    public void shouldReturnSumGivenTwoNumbers() {
+    public void shouldReturnSumGivenTwoNumbers() throws Exception {
         assertThat(StringCalculator.calculate("1,2"), is(3));
-        assertThat(StringCalculator.calculate("-1,-2"), is(-3));
-        assertThat(StringCalculator.calculate("123,-1"), is(122));
+        assertThat(StringCalculator.calculate("123,1"), is(124));
+    }
+
+    @Test
+    public void shouldReturnSumGivenMoreThanTwoNumbers() throws Exception {
+        assertThat(StringCalculator.calculate("0,1,456"), is(457));
+    }
+
+    @Test
+    public void shouldReturnSumGivenNumbersSplitedByCommaOrNewLine() throws Exception {
+        assertThat(StringCalculator.calculate("0\n89"), is(89));
+        assertThat(StringCalculator.calculate("0\n89,1"), is(90));
+    }
+
+    @Test
+    public void shouldReturnSumGivenSpecifiedDelimiter() throws Exception {
+        assertThat(StringCalculator.calculate(";\n0;1;1"), is(2));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldThrowExceptionWhenSumGivenOneNegativeNumber() throws Exception {
+        exception.expect(Exception.class);
+        exception.expectMessage(containsString("negatives not allowed"));
+        exception.expectMessage(containsString("-1"));
+        StringCalculator.calculate("-1,1");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSumGivenMoreThanOneNegativeNumbers() throws Exception {
+        exception.expect(Exception.class);
+        exception.expectMessage(containsString("negatives not allowed"));
+        exception.expectMessage(containsString("-1, -1"));
+        StringCalculator.calculate("-1,-1");
+
+        exception.expectMessage(containsString("-1, -1"));
+        StringCalculator.calculate("-1,2,-1");
     }
 }
